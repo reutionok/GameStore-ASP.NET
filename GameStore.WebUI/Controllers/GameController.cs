501a8c80096ad3fa51f5dc72e8afba968d43d694
reutionok <reutionok@users.noreply.github.com>
2020-07-19 20:43:02 +0300

    add page navigation

--- GameStore.WebUI/Controllers/GameController.cs
+++ GameStore.WebUI/Controllers/GameController.cs
@@ -17,11 +17,12 @@ namespace GameStore.WebUI.Controllers
         {
             repository = repo;
         }
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category, int page = 1)
         {
             GamesListViewModel model = new GamesListViewModel
             {
                 Games = repository.Games
+                       .Where(p => category == null || p.Category == category)
                      .OrderBy(game => game.GameId)
                      .Skip((page - 1) * pageSize)
                      .Take(pageSize),
@@ -29,8 +30,11 @@ namespace GameStore.WebUI.Controllers
                 {
                     CurrentPage = page,
                     ItemsPerPage = pageSize,
-                    TotalItems = repository.Games.Count()
-                }
+                    TotalItems = category == null ?
+                     repository.Games.Count() :
+                    repository.Games.Where(game => game.Category == category).Count()
+                },
+                CurrentCategory = category
             };
             return View(model);
         }
--- GameStore.WebUI/Models/GamesListViewModel.cs
+++ GameStore.WebUI/Models/GamesListViewModel.cs
@@ -8,5 +8,6 @@ namespace GameStore.WebUI.Models
     {
         public IEnumerable<Game> Games { get; set; }
         public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
     }
 }
